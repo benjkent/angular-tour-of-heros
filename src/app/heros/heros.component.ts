a59c6c2cfbc9d51eba6071b1a888c5b7bc49f68a
Benjamin Kent <benjaminfranklinkent@gmail.com>
2022-11-19 13:14:18 -0800

    Add mock data and update ui

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
