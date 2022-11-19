af59586b15cb39c207469b1a116262d9dec6ae89
Benjamin Kent <benjaminfranklinkent@gmail.com>
2022-11-19 14:40:20 -0800

    Add HeroService to access data

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'
+
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
@@ -8,16 +10,22 @@ import { HEROES } from '../mock-heroes'
 })
 export class HerosComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(
+          private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
 }
